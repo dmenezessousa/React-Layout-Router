89cdbec12b23f28afd0d6b8dae8614d3501379f6
Diego Sousa <khesanh@diegos-mbp.myfiosgateway.com>
2022-02-09 09:24:35 -0500

    Layout-Router

---
+++ src/components/Pages/HomePage.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import Layout from '../layout/Layout'
+
+function HomePage() {
+    return (
+        <div>
+            <Layout>
+                <div>
+                    <h1>Home Page</h1>
+                    <p>lorum batos lorum batos lorum batos lorum batos lorum batos lorum batos lorum batos lorum batos.</p>
+                </div>
+            </Layout>        
+        </div>
+    )
+}
+
+export default HomePage
---
+++ src/components/Pages/SignIn.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Layout from '../layout/Layout'
+
+function SignIn() {
+    return (
+        <div>
+            <Layout>
+                <div>
+                    <h1>Sign In Page</h1>
+                </div>
+            </Layout>
+        </div>
+    )
+}
+
+export default SignIn
